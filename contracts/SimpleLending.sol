4aca08bf9cc95a45f1363b51873e69896a40063e
havi-kim <hyochankim94@gmail.com>
2023-07-05 01:57:02 +0900

    feat: Add simple lending

---
+++ contracts/SimpleLending.sol
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "./libraries/Math.sol";
+
+import "../interfaces/IERC20.sol";
+import "../interfaces/ILending.sol";
+import "../interfaces/IPriceOracle.sol";
+
+/// @title Simple Lending
+contract SimpleLending is ILending {
+    struct Position {
+        uint debtAmount;
+        uint collateralAmount;
+    }
+
+    struct Asset {
+        uint debtFactor; // 100% =  1e4
+        uint collateralFactor; // 100% = 1e4
+    }
+
+    mapping(address => Asset) public assets;
+    mapping(bytes32 => Position) public positions;
+    IPriceOracle public priceOracle;
+
+    function deposit(address _debt, address _collateral, uint _collateralDelta) external {
+        // 0. Copy position
+        bytes32 positionKey = getPositionKey(msg.sender, _debt, _collateral);
+        Position memory position = positions[positionKey];
+
+        // 1. Increase collateral amount
+        position.collateralAmount += _collateralDelta;
+
+        // 2. Transfer collateral from sender
+        IERC20(_collateral).transferFrom(msg.sender, address(this), _collateralDelta);
+
+        // 3. Store position changes
+        positions[positionKey] = position;
+    }
+
+    function withdraw(address _debt, address _collateral, uint _collateralDelta) external {
+        // 0. Copy position
+        bytes32 positionKey = getPositionKey(msg.sender, _debt, _collateral);
+        Position memory position = positions[positionKey];
+
+        // 1. Decrease collateral amount & health check
+        position.collateralAmount -= _collateralDelta;
+        healthCheck(position, _debt, _collateral);
+
+        // 2. Transfer collateral from sender
+        IERC20(_collateral).transfer(msg.sender, _collateralDelta);
+
+        // 3. Store position changes
+        positions[positionKey] = position;
+
+    }
+
+    function borrow(address _debt, address _collateral, uint _debtDelta) external {
+        // 0. Copy position
+        bytes32 positionKey = getPositionKey(msg.sender, _debt, _collateral);
+        Position memory position = positions[positionKey];
+
+        // 1. Increase collateral amount & health check
+        position.debtAmount += _debtDelta;
+        healthCheck(position, _debt, _collateral);
+
+        // 2. Transfer collateral from sender
+        IERC20(_debt).transfer(msg.sender, _debtDelta);
+
+        // 3. Store position changes
+        positions[positionKey] = position;
+    }
+
+    function repay(address _debt, address _collateral, uint _debtDelta) external {
+        // 0. Copy position
+        bytes32 positionKey = getPositionKey(msg.sender, _debt, _collateral);
+        Position memory position = positions[positionKey];
+
+        // 1. Increase collateral amount
+        position.debtAmount -= _debtDelta;
+
+        // 2. Transfer collateral from sender
+        IERC20(_debt).transferFrom(msg.sender, address(this), _debtDelta);
+
+        // 3. Store position changes
+        positions[positionKey] = position;
+    }
+
+    function liquidate(address _user, address _debt, address _collateral) external {}
+
+    function healthCheck(
+        Position memory _position,
+        address _debt,
+        address _collateral
+    ) public returns (bool){
+        uint debtValue = Math.mulDiv(
+            priceOracle.getPrice(_debt),
+            _position.debtAmount,
+            1e18
+        );
+        uint debtCredit = Math.mulDiv(
+            debtValue,
+            assets[_debt].debtFactor,
+            1e14
+        );
+        uint collateralValue = Math.mulDiv(
+            priceOracle.getPrice(_collateral),
+            _position.collateralAmount,
+            1e18
+        );
+        uint collateralCredit = Math.mulDiv(
+            collateralValue,
+            assets[_collateral].collateralFactor,
+            1e14
+        );
+        return debtCredit < collateralCredit;
+    }
+
+    function getPositionKey(address _user, address _debt, address _collateral) internal returns (bytes32){
+        return keccak256(abi.encode(_user, _debt, _collateral));
+    }
+}
