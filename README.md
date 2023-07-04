991ce1a5cad171ea030376ae062305b39fd83e8f
havi-kim <hyochankim94@gmail.com>
2023-07-05 01:56:28 +0900

    chore: Initial setting

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Internship_assignment
+# Internship assignment
+
+## Installation:
+
+1) Clone Factorial v1-core
+```
+$ git clone https://github.com/factorial-build/Internship_assignment
+$ cd Internship_assignment
+```
+
+2) Install dependencies
+```
+$ yarn install
+```
+
+3) Runt tests
+```
+$ yarn hardhat test
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,49 @@
+import { HardhatUserConfig } from "hardhat/types";
+
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-waffle'
+import 'hardhat-typechain'
+import 'hardhat-watcher'
+import "hardhat-gas-reporter";
+import "hardhat-contract-sizer";
+
+const DEFAULT_COMPILER_SETTINGS = {
+  version: '0.8.18',
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+    metadata: {
+      bytecodeHash: 'none',
+    },
+  },
+}
+
+const config: HardhatUserConfig = {
+  networks: {
+      hardhat: {
+          mining: {
+              auto: true,
+              interval: 2000,
+          },
+          forking: {
+              url: "https://polygon-rpc.com"
+          }
+      },
+      polygon: {
+          url: "https://polygon-rpc.com"
+      },
+  },
+  solidity: {
+    compilers: [DEFAULT_COMPILER_SETTINGS]
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+}
+
+export default config;
